77311725affd0bdc7e9c73eea942350a887b2516
dante <dantinr@gmail.com>
2020-10-16 11:46:48 +0800

    分类管理

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -14,4 +14,5 @@ Route::group([
     $router->get('/', 'HomeController@index')->name('home');
     $router->resource('users', UserController::class);
     $router->resource('goods', GoodsController::class);     //商品管理
+    $router->resource('category', CategoryController::class);   //分类管理
 });
