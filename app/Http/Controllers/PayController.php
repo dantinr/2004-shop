f64ab04277f2c6002a442a5a5fd078758d459661
dante <dantinr@gmail.com>
2020-10-21 10:56:33 +0800

    支付宝支付拼接参数

--- app/Http/Controllers/PayController.php
+++ app/Http/Controllers/PayController.php
@@ -17,8 +17,32 @@ class PayController extends Controller
 
         //根据订单号，查询订单信息，验证订单是否有效（未支付、未删除、未过期）
 
+
+
         //组合参数，调用支付接口，支付
 
+        $param0 = [             // 请求参数（订单相关）
+
+        ],
+
+        $param1 = [
+            'app_id'    => env('ALIPAY_APP_ID'),
+            'method'    => 'alipay.trade.page.pay',
+            'return_url'    => '',           // 支付成功同步回跳地址
+            'charset'       => 'utf-8',
+            'sign_type'     => 'RSA2',
+            'sign'          => '',              //数据签名
+            'timestamp'     => date('Y-m-d H:i:s'),
+            'version'       => '1.0',
+            'notify_url'    => env('ALIPAY_NOTIFY_URL'),        //支付成功异步通知地址
+            'biz_content'   => ''
+        ];
+
+
+
+        $url = 'https://openapi.alipaydev.com/gateway.do?xxxxx';
+
+        return redirect($url);
     }
 
 
