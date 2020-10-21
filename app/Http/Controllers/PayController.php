33c89bb81ace33c59f33fb132464e594da86c767
dante <dantinr@gmail.com>
2020-10-21 11:36:49 +0800

    支付宝签名

--- app/Http/Controllers/PayController.php
+++ app/Http/Controllers/PayController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class PayController extends Controller
 {
@@ -21,28 +22,62 @@ class PayController extends Controller
 
         //组合参数，调用支付接口，支付
 
-        $param0 = [             // 请求参数（订单相关）
+        // 1 请求参数
+        $param2 = [
+            'out_trade_no'      => $oid,     //商户订单号
+            'product_code'      => 'FAST_INSTANT_TRADE_PAY',
+            'total_amount'      => 0.01,    //订单总金额
+            'subject'           => '2004-测试订单-'.Str::random(16),
+        ];
 
-        ],
-
+        // 2 公共参数
         $param1 = [
-            'app_id'    => env('ALIPAY_APP_ID'),
-            'method'    => 'alipay.trade.page.pay',
-            'return_url'    => '',           // 支付成功同步回跳地址
+            'app_id'        => env('ALIPAY_APP_ID'),
+            'method'        => 'alipay.trade.page.pay',
+            'return_url'    => 'https://1910liwei.comcto.com/pay/alireturn',   //同步通知地址 真实服务器URL
             'charset'       => 'utf-8',
             'sign_type'     => 'RSA2',
-            'sign'          => '',              //数据签名
             'timestamp'     => date('Y-m-d H:i:s'),
             'version'       => '1.0',
-            'notify_url'    => env('ALIPAY_NOTIFY_URL'),        //支付成功异步通知地址
-            'biz_content'   => ''
+            'notify_url'    => 'https://1910liwei.comcto.com/pay/alinotify',   // 异步通知 真实服务器URL
+            'biz_content'   => json_encode($param2),
         ];
 
 
 
-        $url = 'https://openapi.alipaydev.com/gateway.do?xxxxx';
+        // 计算签名
+        ksort($param1);
 
+        $str = "";
+        foreach($param1 as $k=>$v)
+        {
+            $str .= $k . '=' . $v . '&';
+        }
+        $str = rtrim($str,'&');     // 拼接待签名的字符串
+        $sign = $this->aliSign($str);
+
+        //沙箱测试地址
+        $url = 'https://openapi.alipaydev.com/gateway.do?'.$str.'&sign='.urlencode($sign);
         return redirect($url);
+    }
+
+
+    /**
+     * 支付宝签名
+     * @param $data
+     * @return string
+     */
+    protected function aliSign($data)
+    {
+        $priKey = file_get_contents(storage_path('keys/priv.key'));
+        $res = openssl_get_privatekey($priKey);
+
+        ($res) or die('您使用的私钥格式错误，请检查RSA私钥配置');
+
+        openssl_sign($data, $sign, $res, OPENSSL_ALGO_SHA256);
+        openssl_free_key($res);
+        $sign = base64_encode($sign);
+        return $sign;
     }
 
 
